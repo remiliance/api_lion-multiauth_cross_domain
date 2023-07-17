f84c25bb22605d72bac172b3db8e397b94bf02ce
RemiPoly <remiliance@yahoo.fr>
2023-07-17 16:40:25 +0200

    Add Post request with CSRF Token

--- api/views.py
+++ api/views.py
@@ -5,20 +5,26 @@ from drf_yasg2 import openapi
 from drf_yasg2.utils import swagger_auto_schema
 
 
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from django.http import JsonResponse
 
 class Lion(viewsets.ViewSet):
-    authentication_classes = [SessionAuthentication]
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     @swagger_auto_schema()
     @action(detail=False, methods=["GET"])
     def test_lion(self, request):
-        print("tt")
+        print(request.COOKIES)
+        print(getattr(request._request, 'user', None))
         data = {"status": "ca marche, Lion!"}
         return JsonResponse(data, safe=False)
 
 
+    @swagger_auto_schema()
+    @action(detail=False, methods=["POST"])
+    def test_lion_post(self, request):
+        data = {"status": "ca marche Post POST!"}
+        return JsonResponse(data, safe=False, status=200)
